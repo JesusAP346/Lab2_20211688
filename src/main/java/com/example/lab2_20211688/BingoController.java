befe06d0674695cc823ec2eeda256f4a6c210e87
Jesus Alvarado <a2021188@pucp.edu.pe>
2024-08-29 20:51:19 -0500

    commit final profe tenga piedad

--- src/main/java/com/example/lab2_20211688/BingoController.java
+++ src/main/java/com/example/lab2_20211688/BingoController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @Controller
 @RequestMapping(value={"/home","/",""},method= RequestMethod.GET)
 public class BingoController {
+
     @GetMapping({"/","/formulario",""})
     public String mostrarConfiguracionJuego(){
         return "configuracion";
