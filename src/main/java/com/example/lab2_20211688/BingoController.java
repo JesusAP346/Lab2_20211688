c37bb0e303b721b979234857f642a84975433c65
Jesus Alvarado <a2021188@pucp.edu.pe>
2024-08-29 19:35:29 -0500

    funciona enviar parametros de vista a controlador

--- src/main/java/com/example/lab2_20211688/BingoController.java
+++ src/main/java/com/example/lab2_20211688/BingoController.java
@@ -1,9 +1,7 @@
 package com.example.lab2_20211688;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping(value={"/home","/",""},method= RequestMethod.GET)
@@ -12,4 +10,12 @@ public class BingoController {
     public String mostrarConfiguracionJuego(){
         return "configuracion";
     }
+    @PostMapping({"/formularioPost"})
+    public String formularioPost(@RequestParam("tamanho") String tamanho, @RequestParam("numeroTarjetas") String numeroTarjetas){
+        //imprimimos en terminal para verificar que llega al controlador
+        System.out.println(tamanho);
+        System.out.println(numeroTarjetas);
+        return "bingo";
+    }
+
 }
