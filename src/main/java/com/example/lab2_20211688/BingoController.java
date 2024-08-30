92b828391d9c327d9dfd146970c9ebedd42ca348
Jesus Alvarado <a2021188@pucp.edu.pe>
2024-08-29 19:59:16 -0500

    funciona cantidad de tarjetas

--- src/main/java/com/example/lab2_20211688/BingoController.java
+++ src/main/java/com/example/lab2_20211688/BingoController.java
@@ -2,6 +2,11 @@ package com.example.lab2_20211688;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Controller
 @RequestMapping(value={"/home","/",""},method= RequestMethod.GET)
@@ -11,10 +16,18 @@ public class BingoController {
         return "configuracion";
     }
     @PostMapping({"/formularioPost"})
-    public String formularioPost(@RequestParam("tamanho") String tamanho, @RequestParam("numeroTarjetas") String numeroTarjetas){
+    public String formularioPost(Model model,@RequestParam("tamanho") String tamanho, @RequestParam("numeroTarjetas") Integer numeroTarjetas){
         //imprimimos en terminal para verificar que llega al controlador
         System.out.println(tamanho);
         System.out.println(numeroTarjetas);
+
+        ArrayList<Integer> listaTarjetas = new ArrayList<>();
+        for(int i=0;i<numeroTarjetas;i++){
+            listaTarjetas.add(i);
+        }
+        model.addAttribute("listaTarjetas",listaTarjetas);
+
+
         return "bingo";
     }
 
